4dbde30a7084bbfd51b996bbc357d278961303ba
Shivangi <shivangi.chhabra@cerner.com>
2018-01-23 22:29:27 -0600

    updated code to save parent as well

--- src/Graph/DijkstraShortestPath.java
+++ src/Graph/DijkstraShortestPath.java
@@ -12,11 +12,14 @@ public class DijkstraShortestPath {
     public void dijkstra(int[][] graph, int src){
         int[] dist = new int[V];
         boolean[] visited = new boolean[V];
+        int[] parent = new int[V];
 
+        parent[0] = -1;
         dist[src] = 0;
 
-        for(int i=src+1; i<V; i++)
+        for(int i=src+1; i<V; i++) {
             dist[i] = Integer.MAX_VALUE;
+        }
 
         // Find shortest path for all vertices
         for(int i=0; i<V-1; i++){
@@ -25,18 +28,20 @@ public class DijkstraShortestPath {
 
             visited[u] = true;
             for(int v=0; v<V; v++){
-                if(!visited[v] && graph[u][v] != 0 && dist[v] > dist[u] + graph[u][v])
+                if(!visited[v] && graph[u][v] != 0 && dist[v] > dist[u] + graph[u][v]) {
+                    parent[v] = u;
                     dist[v] = dist[u] + graph[u][v];
+                }
             }
         }
-        printSolution(dist, V);
+        printSolution(dist, parent);
     }
 
     public int minDistance(int[] dist, boolean[] visited) {
         int min = Integer.MAX_VALUE;
-        int minIndex = -1;
+        int minIndex = 0;
         for (int i = 0; i < V; i++) {
-            if (!visited[i] && dist[i] < min) {
+            if (!visited[i] && dist[i] <= min) {
                 min = dist[i];
                 minIndex = i;
             }
@@ -44,11 +49,23 @@ public class DijkstraShortestPath {
         return minIndex;
     }
 
-    public void printSolution(int dist[], int n)
+    public void printSolution(int dist[], int[] parent)
     {
-        System.out.println("Vertex   Distance from Source");
-        for (int i = 0; i < V; i++)
-            System.out.println(i+" \t\t "+dist[i]);
+        System.out.println("Vertex \t\tDistance \t\tPath");
+        int src = 0;
+        for (int i = 1; i < V; i++) {
+            System.out.print(src+ "->" +i + " \t\t " + (dist[i] > 9 ? dist[i] : dist[i]+" ") + " \t\t\t " + src + " ");
+            printPath(parent, i);
+            System.out.println();
+        }
+    }
+
+    public void printPath(int[] parent, int i){
+        if(parent[i] == -1)
+            return;
+
+        printPath(parent, parent[i]);
+        System.out.print(i + " ");
     }
 
     public static void main(String args[]){
@@ -57,13 +74,12 @@ public class DijkstraShortestPath {
                         {0, 8, 0, 7, 0, 4, 0, 0, 2},
                         {0, 0, 7, 0, 9, 14, 0, 0, 0},
                         {0, 0, 0, 9, 0, 10, 0, 0, 0},
-                        {0, 0, 4, 14, 10, 0, 2, 0, 0},
-                        {0, 0, 0, 0, 0, 2, 0, 1, 6},
+                        {0, 0, 4, 0, 10, 0, 2, 0, 0},
+                        {0, 0, 0, 14, 0, 2, 0, 1, 6},
                         {8, 11, 0, 0, 0, 0, 1, 0, 7},
                         {0, 0, 2, 0, 0, 0, 6, 7, 0}
         };
         DijkstraShortestPath p = new DijkstraShortestPath();
         p.dijkstra(graph, 0);
-
     }
 }
